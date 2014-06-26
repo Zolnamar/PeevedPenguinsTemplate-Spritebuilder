090574ae65615179b17a112fba119fb8eafae912
Zolnamar Dorjsembe <zolnamar@Zolnamars-MacBook-Pro.local>
2014-06-26 12:17:12 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Zolnamar Dorjsembe on 6/26/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
