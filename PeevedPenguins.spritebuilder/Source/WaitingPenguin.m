f998d88b2be89ed71c299c81244efaa575ce350b
Zolnamar Dorjsembe <zolnamar@zolnamars-mbp.att.net>
2014-06-27 01:13:18 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/WaitingPenguin.m
+++ PeevedPenguins.spritebuilder/Source/WaitingPenguin.m
@@ -18,4 +18,12 @@
     [self performSelector:@selector(startBlinkAndJump) withObject:nil afterDelay:delay];
 }
 
+- (void)startBlinkAndJump
+{
+    // the animation manager of each node is stored in the 'animationManager' property
+    CCAnimationManager* animationManager = self.animationManager;
+    // timelines can be referenced and run by name
+    [animationManager runAnimationsForSequenceNamed:@"BlinkAndJump"];
+}
+
 @end
