6a749b225657ddd99ab3ca0515aad9481487a0da
Zolnamar Dorjsembe <zolnamar@zolnamars-mbp.att.net>
2014-06-27 01:08:36 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -21,26 +21,21 @@
     CCPhysicsJoint *_penguinCatapultJoint;
 }
 
-// is called when CCB file has completed loading
-- (void)didLoadFromCCB {
-    // tell this scene to accept touches
-    self.userInteractionEnabled = TRUE;
-    CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
-    [_levelNode addChild:level];
-   
-    // visualize physics bodies & joints
-    _physicsNode.debugDraw = TRUE;
-    
-    
-    // nothing shall collide with our invisible nodes
-    _pullbackNode.physicsBody.collisionMask = @[];
-    _mouseJointNode.physicsBody.collisionMask = @[];
-    
-    
-    _physicsNode.collisionDelegate = self;
-    
+- (void)didLoadFromCCB
+{
+    // generate a random number between 0.0 and 2.0
+    float delay = (arc4random() % 2000) / 1000.f;
+    // call method to start animation after random delay
+    [self performSelector:@selector(startBlinkAndJump) withObject:nil afterDelay:delay];
 }
 
+- (void)startBlinkAndJump
+{
+    // the animation manager of each node is stored in the 'animationManager' property
+    CCAnimationManager* animationManager = self.animationManager;
+    // timelines can be referenced and run by name
+    [animationManager runAnimationsForSequenceNamed:@"BlinkAndJump"];
+}
 
 
 
